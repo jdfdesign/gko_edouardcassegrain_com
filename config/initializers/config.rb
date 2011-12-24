c2f327797101a7bb8c0d19f6eeb05c33e5c9b38c
jdfdesign <regis@joufdesign.com>
2011-12-24 12:15:25 -0430

    precompile

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,2 +1,2 @@
 APP_CONFIG = YAML.load_file("config/config.yml")[Rails.env]
-GkoCassegrainCom::Application.config.to_prepare { require_dependency 'game_list' }
+#GkoCassegrainCom::Application.config.to_prepare { require_dependency 'game_list' }
