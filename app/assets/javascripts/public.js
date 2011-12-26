6d20f0de46c893c7c1e05c19f8348e4114c2a600
jdfdesign <regis@joufdesign.com>
2011-12-26 13:11:54 -0430

    precompile

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -10,13 +10,22 @@
 
 
 $(document).ready(function() {
-	Gallery.init($('.images:first'));
+	Gallery.init($('.images:first')); 
+	if (jQuery.browser.msie) {
+		$('.ec-circle h3').css({color:'#fff', display:'none'});
+	}
 	$('.circle').circlemouse({
 		onMouseEnter	: function( el ) {
 			el.addClass('ec-circle-hover');
+			if (jQuery.browser.msie) {
+				el.find('h3:first').stop().fadeIn();
+			}
 		},
 		onMouseLeave : function( el ) {
 			el.removeClass('ec-circle-hover');
+			if (jQuery.browser.msie) {
+				el.find('h3:first').stop().fadeOut();
+			}
 		},
 		onClick : function( el ) {
 			//$loader.show();
